946cfc96736c318d6966c2f5b4b60fb0b82f28a1
Khushi Yadav <kky957242@gmail.com>
2024-12-16 23:06:17 +0530

    Create sum.cpp

---
+++ sum.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int a, b;
+    cout << "Enter two numbers: ";
+    cin >> a >> b; // Input two numbers
+    cout << "Sum is: " << a + b << endl; // Display their sum
+    return 0;
+}
