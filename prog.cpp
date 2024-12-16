b6da2e7f82d38791dc61b0761d3464167bebe591
Khushi Yadav <kky957242@gmail.com>
2024-12-16 23:06:55 +0530

    Create prog.cpp

---
+++ prog.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int age = 20;
+    float height = 5.8;
+    char grade = 'A';
+    bool isStudent = true;
+
+    cout << "Age: " << age << endl;
+    cout << "Height: " << height << endl;
+    cout << "Grade: " << grade << endl;
+    cout << "Is Student: " << isStudent << endl;
+
+    return 0;
+}
